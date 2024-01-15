1a1798f09a01e8adcf5e8c0c3b150f4365fb526b
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-15 16:40:36 -0700

    add placeShip method. Need to implement vertical alignment

--- battleship.js
+++ battleship.js
@@ -1,12 +1,37 @@
-function Ship(length) {
+function Ship(length, name) {
     return {
+        name: name,
         length: length,
-        beenHit: beenHit,
+        beenHit: 0,
         hit: function () {
             this.beenHit++;
         },
         isSunk: function() {
             return this.length <= this.beenHit;
+        }
+    }
+}
+
+export function Gameboard() {
+    return {
+        board: [[null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null, null, null], 
+        [null, null, null, null, null, null, null, null, null, null]],
+
+        placeShip: function(x, y, length, alignment, shipName) {
+            const ship = Ship(length, shipName);
+            if (alignment === 'horizontal') {
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[y][x + i] = ship.name;
+                }
+            }
         }
     }
 }
