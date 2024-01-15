c6442f574a90f33bb6e7ab64762fecbd44a47db3
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-15 16:40:53 -0700

    tests for gameboard

--- battleship.test.js
+++ battleship.test.js
@@ -1,1 +1,32 @@
 import {describe, expect, test} from '@jest/globals';
+import { Gameboard } from './battleship';
+
+test('Gameboard', () => {
+    expect(Gameboard().board).toEqual([[null, null, null, null, null, null, null, null, null, null], 
+                                       [null, null, null, null, null, null, null, null, null, null],
+                                       [null, null, null, null, null, null, null, null, null, null], 
+                                       [null, null, null, null, null, null, null, null, null, null],
+                                       [null, null, null, null, null, null, null, null, null, null], 
+                                       [null, null, null, null, null, null, null, null, null, null],
+                                       [null, null, null, null, null, null, null, null, null, null], 
+                                       [null, null, null, null, null, null, null, null, null, null],
+                                       [null, null, null, null, null, null, null, null, null, null], 
+                                       [null, null, null, null, null, null, null, null, null, null]])
+})
+
+test('Gameboard ship placement', () => {
+    const gameboard = Gameboard();
+    gameboard.placeShip(3, 4, 3, 'horizontal', 'ship1');
+    expect(gameboard.board).toEqual( [[null, null, null, null, null, null, null, null, null, null], 
+                                        [null, null, null, null, null, null, null, null, null, null],
+                                        [null, null, null, null, null, null, null, null, null, null], 
+                                        [null, null, null, null, null, null, null, null, null, null],
+                                        [null, null, null, 'ship1', 'ship1', 'ship1', null, null, null, null], 
+                                        [null, null, null, null, null, null, null, null, null, null],
+                                        [null, null, null, null, null, null, null, null, null, null], 
+                                        [null, null, null, null, null, null, null, null, null, null],
+                                        [null, null, null, null, null, null, null, null, null, null], 
+                                        [null, null, null, null, null, null, null, null, null, null]]
+
+        )
+})
