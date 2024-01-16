a42bee8b4c17749417e390f7e8485966c7b88f0b
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-16 14:49:38 -0700

    display player and computer boards

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ function gameDisplay() {
     const player = Player('1');
     const computer = Player('computer');
 
-    const container = document.querySelector('.container');
+    const playerContainer = document.querySelector('.container.player');
     player.playerBoard.board.forEach(row => {
         row.forEach(cell => {
             console.log('blah')
             const cellDiv = document.createElement('div');
             cellDiv.classList.add('cell');
-            container.appendChild(cellDiv);
+            playerContainer.appendChild(cellDiv);
+        })
+    })
+
+    const computerContainer = document.querySelector('.container.computer');
+    computer.playerBoard.board.forEach(row => {
+        row.forEach(cell => {
+            console.log('blah')
+            const cellDiv = document.createElement('div');
+            cellDiv.classList.add('cell');
+            computerContainer.appendChild(cellDiv);
         })
     })
 }
+
 
 gameDisplay();
