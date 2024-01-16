74a8a7f9899d1d505eac0e3a33d1cc2f57f3cc22
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-16 10:40:13 -0700

    Add Ship factory, Gameboard factory funcntions

--- battleship.js
+++ battleship.js
@@ -1,36 +1,38 @@
-function Ship(length, name) {
+export function Ship(length) {
     return {
-        name: name,
         length: length,
         beenHit: 0,
         hit: function () {
             this.beenHit++;
         },
         isSunk: function() {
-            return this.length <= this.beenHit;
+            return this.beenHit >= this.length;
         }
     }
 }
 
 export function Gameboard() {
     return {
-        board: [[null, null, null, null, null, null, null, null, null, null], 
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null], 
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null], 
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null], 
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null], 
-        [null, null, null, null, null, null, null, null, null, null]],
+        board: [...Array(10)].map(e => Array(10).fill(null)),
 
-        placeShip: function(x, y, length, alignment, shipName) {
-            const ship = Ship(length, shipName);
+        playedBoard: [...Array(10)].map(e => Array(10).fill(null)),
+
+        placeShip: function(x, y, alignment, ship) {
             if (alignment === 'horizontal') {
                 for (let i = 0; i < ship.length; i++) {
-                    this.board[y][x + i] = ship.name;
+                    this.board[y][x + i] = ship;
                 }
+            } else {
+                for (let i = 0; i < ship.length; i++) {
+                    this.board[y + i][x] = ship;
+                }
+            }
+        },
+
+        receiveAttack: function(x, y) {
+            if (!this.playedBoard[y][x]) {
+                this.board[y][x] ? this.board[y][x].hit() : this.board[y][x] = 0;
+                this.playedBoard[y][x] = 1;
             }
         }
     }
