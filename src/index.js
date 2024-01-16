01be372fd7a9319aad2fd4b6fe49ce585cd16de0
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-16 16:36:21 -0700

    creates divs and adds divs based on the gameboard

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import { Ship, Gameboard, Player } from './modules/battleship';
+import game from './modules/game';
 
+gameDisplay();
+// game();
 
 function gameDisplay() {
     const player = Player('1');
     const computer = Player('computer');
-
+    player.playerBoard.randomPlace(Ship(5));
+    player.playerBoard.randomPlace(Ship(4));
+    player.playerBoard.randomPlace(Ship(3));
+    player.playerBoard.randomPlace(Ship(3));
+    player.playerBoard.randomPlace(Ship(2));
+    computer.playerBoard.randomPlace(Ship(5));
+    computer.playerBoard.randomPlace(Ship(4));
+    computer.playerBoard.randomPlace(Ship(3));
+    computer.playerBoard.randomPlace(Ship(3));
+    computer.playerBoard.randomPlace(Ship(2));
     const playerContainer = document.querySelector('.container.player');
+
+
     player.playerBoard.board.forEach(row => {
         row.forEach(cell => {
-            console.log('blah')
             const cellDiv = document.createElement('div');
+            if (cell) {
+                cellDiv.classList.add('ship');
+
+            }
             cellDiv.classList.add('cell');
             playerContainer.appendChild(cellDiv);
         })
@@ -18,13 +35,17 @@ function gameDisplay() {
     const computerContainer = document.querySelector('.container.computer');
     computer.playerBoard.board.forEach(row => {
         row.forEach(cell => {
-            console.log('blah')
             const cellDiv = document.createElement('div');
+            if (cell) {
+                cellDiv.classList.add('ship');
+
+            }
             cellDiv.classList.add('cell');
+
             computerContainer.appendChild(cellDiv);
         })
     })
 }
 
 
-gameDisplay();
+
