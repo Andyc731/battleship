6213fd0eee2a1539791fcd747b4aec517a50285f
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-17 13:07:11 -0700

    move functions to game module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import { Ship, Gameboard, Player } from './modules/battleship';
-// import gameLoop from './modules/game';
+import gameLoop from './modules/game';
 
 gameDisplay();
-// game();
 
 function gameDisplay() {
     const player = Player('1');
@@ -55,63 +54,4 @@ function gameDisplay() {
     
     gameLoop(player, computer);
     
-}
-
-async function gameLoop(player, computer) {
-    let turnToggle = true;
-    while (!player.playerBoard.allShipsSunk() && !computer.playerBoard.allShipsSunk()) {
-        if (turnToggle) {
-            const attackCoord = await playerTurn();
-            if (!computer.playerBoard.playedBoard[attackCoord.y][attackCoord.x]) {
-                player.attack(computer.playerBoard, attackCoord.x, attackCoord.y);
-                turnToggle = !turnToggle;
-            }
-        } else {
-            const coordinates = computer.randomCoord(player.playerBoard);
-            computer.attack(player.playerBoard, coordinates.x, coordinates.y)
-            turnToggle = !turnToggle;
-        }
-        displayBoards(player, computer);
-    }
-}
-
-function playerTurn() {
-    return new Promise(resolve=> {
-        const computerCells = document.querySelectorAll('.container.computer .cell');
-
-        function playerMoveHandler(event) {
-            
-            const clickedCell = event.target;
-            const xInput = Number(clickedCell.dataset.x);
-            const yInput = Number(clickedCell.dataset.y);
-
-            computerCells.forEach(cellDiv => {
-                cellDiv.removeEventListener('click', playerMoveHandler);
-            });
-
-            resolve({ x: xInput, y: yInput });
-        }
-
-        computerCells.forEach(cellDiv => {
-            cellDiv.addEventListener('click', playerMoveHandler);
-        });
-    })
-}
-
-function displayBoards(player, computer) {
-    const BOARDLENGTH = 10;
-    for (let i = 0; i < BOARDLENGTH; i++) {
-        for (let j = 0; j < BOARDLENGTH; j++) {
-            if (computer.playerBoard.playedBoard[i][j]) {
-                const cellDiv = document.querySelector(`.computer [data-x="${j}"][data-y="${i}"]`);
-                cellDiv.classList.add('played');
-                
-            }
-            if (player.playerBoard.playedBoard[i][j]) {
-                const cellDiv = document.querySelector(`.player [data-x="${j}"][data-y="${i}"]`);
-                cellDiv.classList.add('played');
-                
-            }
-        }
-    }
 }
--- src/modules/game.js
+++ src/modules/game.js
@@ -1,28 +1,60 @@
-import { Ship, Gameboard, Player } from './battleship';
-
-function game() {
-    const player = Player('player')
-    const computer = Player('computer');
-    let playerTurn = true;
-
-    player.playerBoard.placeShip(2, 3, 'vertical', Ship(4))
-    computer.playerBoard.placeShip(2, 3, 'vertical', Ship(4))
-
-
-
-    // while (!player.playerBoard.allShipsSunk() && !computer.playerBoard.allShipsSunk()) {
-    //     console.log(player, computer)
-    //     if (playerTurn) {
-    //         player.attack(computer.playerBoard, xInput, yInput);
-    //         playerTurn = !playerTurn;
-    //     } else {
-    //         const coordinates = computer.randomCoord(player.playerBoard);
-    //         computer.attack(player.playerBoard, coordinates.x, coordinates.y)
-    //         playerTurn = !playerTurn;
-    //     }
-    // }
-
-
+async function gameLoop(player, computer) {
+    let turnToggle = true;
+    while (!player.playerBoard.allShipsSunk() && !computer.playerBoard.allShipsSunk()) {
+        if (turnToggle) {
+            const attackCoord = await playerTurn();
+            if (!computer.playerBoard.playedBoard[attackCoord.y][attackCoord.x]) {
+                player.attack(computer.playerBoard, attackCoord.x, attackCoord.y);
+                turnToggle = !turnToggle;
+            }
+        } else {
+            const coordinates = computer.randomCoord(player.playerBoard);
+            computer.attack(player.playerBoard, coordinates.x, coordinates.y)
+            turnToggle = !turnToggle;
+        }
+        displayBoards(player, computer);
+    }
 }
 
-export default game;
+function playerTurn() {
+    return new Promise(resolve=> {
+        const computerCells = document.querySelectorAll('.container.computer .cell');
+
+        function playerMoveHandler(event) {
+            
+            const clickedCell = event.target;
+            const xInput = Number(clickedCell.dataset.x);
+            const yInput = Number(clickedCell.dataset.y);
+
+            computerCells.forEach(cellDiv => {
+                cellDiv.removeEventListener('click', playerMoveHandler);
+            });
+
+            resolve({ x: xInput, y: yInput });
+        }
+
+        computerCells.forEach(cellDiv => {
+            cellDiv.addEventListener('click', playerMoveHandler);
+        });
+    })
+}
+
+function displayBoards(player, computer) {
+    const BOARDLENGTH = 10;
+    for (let i = 0; i < BOARDLENGTH; i++) {
+        for (let j = 0; j < BOARDLENGTH; j++) {
+            if (computer.playerBoard.playedBoard[i][j]) {
+                const cellDiv = document.querySelector(`.computer [data-x="${j}"][data-y="${i}"]`);
+                cellDiv.classList.add('played');
+                
+            }
+            if (player.playerBoard.playedBoard[i][j]) {
+                const cellDiv = document.querySelector(`.player [data-x="${j}"][data-y="${i}"]`);
+                cellDiv.classList.add('played');
+                
+            }
+        }
+    }
+}
+
+export default gameLoop;
