f6160929b986ce8965cbd80f89c97d0d73e5883d
Andyc731 <128937683+Andyc731@users.noreply.github.com>
2024-01-17 13:25:13 -0700

    add createBoard function for cleaner code

--- src/index.js
+++ src/index.js
@@ -17,9 +17,18 @@ function gameDisplay() {
     computer.playerBoard.randomPlace(Ship(3));
     computer.playerBoard.randomPlace(Ship(2));
 
-    const BOARDLENGTH = 10;
     const playerContainer = document.querySelector('.container.player');
+    
+    createBoard(player, '.container.player');
+    createBoard(computer, '.container.computer');
+    
+    gameLoop(player, computer);
+    
+}
 
+function createBoard(player, containerSelector) {
+    const BOARDLENGTH = 10;
+    const container = document.querySelector(containerSelector);
     for (let i = 0; i < BOARDLENGTH; i++) {
         for (let j = 0; j < BOARDLENGTH; j++) {
             const cellDiv = document.createElement('div');
@@ -31,27 +40,7 @@ function gameDisplay() {
             if (player.playerBoard.board[i][j]) {
                 cellDiv.classList.add('ship');
             }
-            playerContainer.appendChild(cellDiv);
+            container.appendChild(cellDiv);
         }
     }
-
-    const computerContainer = document.querySelector('.container.computer');
-    for (let i = 0; i < BOARDLENGTH; i++) {
-        for (let j = 0; j < BOARDLENGTH; j++) {
-            const cellDiv = document.createElement('div');
-            cellDiv.classList.add('cell');
-
-            cellDiv.dataset.x = j;
-            cellDiv.dataset.y = i;
-
-            if (computer.playerBoard.board[i][j]) {
-                cellDiv.classList.add('ship');
-            }
-            // cellEventListener(cellDiv, player, computer);
-            computerContainer.appendChild(cellDiv);
-        }
-    }
-    
-    gameLoop(player, computer);
-    
 }
